057026a0f9619697e6deb681b80dbd282df36ff2
dingxuewu <2497436726@qq.com>
2021-07-20 16:15:09 +0800

    detail-ajax

--- travel/src/pages/detail/Detail.vue
+++ travel/src/pages/detail/Detail.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <DetailBanner/>
-    <DetailHeader/>
+    <detail-banner
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :bannerImgs="gallaryImgs"
+    ></detail-banner>
+    <detail-header></detail-header>
     <div class="content">
-    <DetailList :list = 'list'/>
+      <detail-list :list="list"></detail-list>
     </div>
   </div>
 </template>
+
 <script>
-import DetailBanner from './components/Banner.vue'
-import DetailHeader from './components/Header.vue'
-import DetailList from './components/List.vue'
+import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
@@ -20,25 +26,38 @@ export default {
   },
   data () {
     return {
-      list: [{
-        title: '成人票',
-        children: [{
-          title: '成人三馆联票'
-        }, {
-          title: '成人五馆联票'
-        }]
-      }, {
-        title: '学生票'
-      }, {
-        title: '儿童票'
-      }, {
-        title: '特惠票'
-      }]
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
     }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
+
 <style lang="stylus" scoped>
-.content
-  height 200vw
+  .content
+    height: 50rem
 </style>
--- travel/src/pages/detail/components/Banner.vue
+++ travel/src/pages/detail/components/Banner.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="https://youimg1.c-ctrip.com/target/01032120008oxvoqa16D3_D_750_420.jpg?proc=autoorient" />
+      <img class="banner-img" :src="bannerImg" />
       <div class="banner-info">
         <div class="banner-tittle">
-          长沙生态动物园(AAAA景区)
+          {{this.sightName}}
         </div>
         <div class="banner-number">
           <span class="iconfont banner-icon">&#xe604;</span>
-          3
+          {{this.bannerImgs.length}}
         </div>
       </div>
     </div>
     <common-gallary
-      :imgs="imgs"
+      :imgs="bannerImgs"
       v-show="showGallary"
       @close="handleGallaryClose"
     ></common-gallary>
@@ -24,10 +24,14 @@
 import CommonGallary from 'common/gallary/Gallary'
 export default {
   name: 'DetailBanner',
+  props: {
+    sightName: String,
+    bannerImg: String,
+    bannerImgs: Array
+  },
   data () {
     return {
-      showGallary: false,
-      imgs: ['https://youimg1.c-ctrip.com/target/01032120008oxvoqa16D3.jpg', 'https://youimg1.c-ctrip.com/target/100313000000v8lqh970A.jpg', 'https://youimg1.c-ctrip.com/target/030691200087hxt4mC533_C_380_286.jpg?proc=autoorient']
+      showGallary: false
     }
   },
   methods: {
@@ -58,21 +62,21 @@ export default {
       left: 0
       right: 0
       bottom: 0
-      line-height: 8vw
+      line-height: .6rem
       color: #fff
       background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
       .banner-tittle
         flex: 1
-        font-size: 4.2667vw
-        padding: 0 2.6667vw
+        font-size: .32rem
+        padding: 0 .2rem
       .banner-number
-        height: 4.2667vw
-        line-height: 4.2667vw
-        margin-top: 1.8667vw
-        padding: 0 5.3333vw
-        border-radius: 2.6667vw
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
         background: rgba(0, 0, 0, .8)
-        font-size: 3.2vw
+        font-size: .24rem
         .banner-icon
-          font-size: .0.032vw
+          font-size: .24rem
 </style>
